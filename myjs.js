378165767fdb76c82cb076c91273c72182c36e54
blarth <jminocente@gmail.com>
2021-11-10 15:17:43 -0300

    requisito chat adicionado

--- myjs.js
+++ myjs.js
@@ -2,26 +2,82 @@ let now = new Date();
 let hora = now.getHours();
 let minutos = now.getMinutes();
 let segundos = now.getSeconds();
-
+requisicaoGet = axios("https://mock-api.driven.com.br/api/v4/uol/messages");
 const mensagemPadraoData = `${hora}:${minutos}:${segundos}`;
 
-let usuario = prompt("Qual o seu lindo nome ?");
+let usuario = { name: prompt("Qual o seu lindo nome ?") };
 
 const localChat = document.querySelector(".chat-uol");
 
-function criaMsgEntrada() {
-  localChat.innerHTML += `<div class="mensagem status">
-        (${hora}:${minutos}:${segundos}) 
-        \u00A0<span>${usuario}</span>\u00A0 
+function trazerMsgsTela(resposta) {
+  const respostaData = resposta.data;
+  console.log(respostaData);
+  for (let i = 0; i < respostaData.length; i++) {
+    switch (respostaData[i].type) {
+      case "status":
+        if (respostaData[i].text === "entra na sala...") {
+          criaMsgEntrada(respostaData[i].from, respostaData[i].time);
+        } else {
+          criaMsgSaida(respostaData[i].from, respostaData[i].time);
+        }
+        break;
+      case "message":
+        criaMsgTodos(
+          respostaData[i].from,
+          respostaData[i].time,
+          respostaData[i].to,
+          respostaData[i].text
+        );
+        break;
+      case "private_message":
+        if (respostaData[i].to === usuario) {
+          criaMsgReservada(
+            respostaData[i].from,
+            respostaData[i].time,
+            respostaData[i].to,
+            respostaData[i].text
+          );
+        } else {
+        }
+        break;
+      default:
+        console.log(respostaData[i].type);
+    }
+  }
+  const ultimaMsg = document.querySelectorAll(".mensagem");
+  ultimaMsg[ultimaMsg.length - 1].scrollIntoView();
+}
+
+function refreshMsgs() {
+  requisicaoGet.then(trazerMsgsTela);
+}
+
+function criaMsgEntrada(usuariox, time) {
+  return (localChat.innerHTML += `<div class="mensagem status">
+        (${time}) 
+        \u00A0<span>${usuariox}</span>\u00A0 
         entrou na sala...
-        </div>`;
+        </div>`);
 }
-function criaMsgSaida() {
-  localChat.innerHTML += `<div class="mensagem status">(${mensagemPadraoData})\u00A0<span>${usuario}</span>\u00A0sai da sala... </div>`;
+
+function criaMsgSaida(usuariox, time) {
+  return (localChat.innerHTML += `<div class="mensagem status">
+(${time})\u00A0<span>${usuariox}</span>\u00A0sai da sala... 
+  </div>`);
 }
-criaMsgEntrada();
-criaMsgSaida();
+/* criaMsgEntrada();
+criaMsgSaida(); */
 /* criaMsgEntrada(); */
-function criaMsgTodos() {
-  localChat.innerHTML += `<div class="mensagem">(${mensagemPadraoData})\u00A0<span>${usuario}</span>\u00A0 para \u00A0<span>Todos</span>\u00A0 ${mensagem} </div>`;
+function criaMsgTodos(usuariox, time, destinatario, mensagemx) {
+  return (localChat.innerHTML += `<div class="mensagem">
+  (${time})\u00A0<span>${usuariox}</span>\u00A0 para \u00A0<span>${destinatario}</span>\u00A0 ${mensagemx} 
+  </div>`);
 }
+
+function criaMsgReservada(usuariox, time, destinatario, mensagemx) {
+  return (localChat.innerHTML += `<div class="mensagem private">
+  (${time})\u00A0<span>${usuariox}</span>\u00A0 para \u00A0<span>${destinatario}</span>\u00A0 ${mensagemx} 
+  </div>`);
+}
+
+setInterval(refreshMsgs, 3000);
