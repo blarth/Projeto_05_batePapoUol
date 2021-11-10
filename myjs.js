4eeeb76aa82e3509abed3902dc51b150f8e5b547
blarth <jminocente@gmail.com>
2021-11-10 15:22:59 -0300

    requisito para facilitar a correcao adicionado

--- myjs.js
+++ myjs.js
@@ -53,7 +53,7 @@ function refreshMsgs() {
 }
 
 function criaMsgEntrada(usuariox, time) {
-  return (localChat.innerHTML += `<div class="mensagem status">
+  return (localChat.innerHTML += `<div class="mensagem status" data-identifier="message">
         (${time}) 
         \u00A0<span>${usuariox}</span>\u00A0 
         entrou na sala...
@@ -61,7 +61,7 @@ function criaMsgEntrada(usuariox, time) {
 }
 
 function criaMsgSaida(usuariox, time) {
-  return (localChat.innerHTML += `<div class="mensagem status">
+  return (localChat.innerHTML += `<div class="mensagem status" data-identifier="message">
 (${time})\u00A0<span>${usuariox}</span>\u00A0sai da sala... 
   </div>`);
 }
@@ -69,15 +69,15 @@ function criaMsgSaida(usuariox, time) {
 criaMsgSaida(); */
 /* criaMsgEntrada(); */
 function criaMsgTodos(usuariox, time, destinatario, mensagemx) {
-  return (localChat.innerHTML += `<div class="mensagem">
+  return (localChat.innerHTML += `<div class="mensagem" data-identifier="message">
   (${time})\u00A0<span>${usuariox}</span>\u00A0 para \u00A0<span>${destinatario}</span>\u00A0 ${mensagemx} 
   </div>`);
 }
 
 function criaMsgReservada(usuariox, time, destinatario, mensagemx) {
-  return (localChat.innerHTML += `<div class="mensagem private">
+  return (localChat.innerHTML += `<div class="mensagem private" data-identifier="message">
   (${time})\u00A0<span>${usuariox}</span>\u00A0 para \u00A0<span>${destinatario}</span>\u00A0 ${mensagemx} 
   </div>`);
 }
-
+requisicaoGet.then(trazerMsgsTela);
 setInterval(refreshMsgs, 3000);
